caa269b2ac9d213f4de215f58748e25eeed38f1e
ECFritzler <ec.fritzler@uleth.ca>
2017-12-05 11:15:39 -0700

    Update peerChat.py
    Should work sending to all peers

--- peerChat.py
+++ peerChat.py
@@ -8,188 +8,145 @@
 # A one line message will be sent to all active users
 # Peer Discovery: To discover peer applications, the receiver
 
-from queue import Queue, Empty
 from threading import Thread, Timer
 from time import sleep
 import threading
 import sys, socket, errno
-import json
+
 
 MYPORT = int(sys.argv[1])
-
 PORTRANGE = [55000, 55001, 55002, 55003, 55004, 55005, 55006, 55007, 55008]
 IPADDRESSRANGE=['142.66.140.186']
 BUFLEN = 1000
 
 for ip in range(21,70):
     IPADDRESSRANGE.append('142.66.140.'+str(ip))
+
 # Receiver class
 # If the reciver receives a "Hello" message, then it must add that
 # user (username) to the list and start the timer.
 class Receiver(Thread):
-    def __init__(self, queue, username):
+    def __init__(self, socket):
         Thread.__init__(self)
-        self.queue = queue
-        self.username = username
+        self.socket = socket
         self.partners = []
 
-    def remove_partner(self):
-        print(self.partners)
+    def remove_partner(self, addr):
+        print(addr)
+        print('remove')
+        for elem in self.partners:
+            if elem[0] == addr:
+                print(elem)
+                print(elem[0])
 
     def run(self):
-        try:
-            s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        except:
-            print ("Cannot open socket")
-            sys.exit(1)
-
-        try:
-            s.bind(('',MYPORT))
-        except:
-            print ("Cannot bind my socket to port")
-            sys.exit(1)
-
         while True:
             try:
-                data, addr = s.recvfrom(BUFLEN)
-                # print(data, addr)
-                #print(addr[0], " + ", addr[1])
-                #print(data)
+                data, addr = self.socket.recvfrom(BUFLEN)
             except OSError as err:
                 print ("Cannot receive from socket: {}".format(err.strerror))
 
             if(data[:5] == b'HELLO'):
-                # If the timer reaches 0, remove the partner from the list
-                # If a hello message is received and the partner is already in
-                # the list, update the timer
                 for elem in self.partners:
                     if elem[0] == addr and elem[1] == data[6:]:
+                        elem[2].cancel()
                         self.partners.remove(elem)
+
                 for IP in IPADDRESSRANGE:
                     for PORT in PORTRANGE:
-                        if((IP, PORT) == addr):
-                            self.partners.append([IP, PORT-1, data[6:]])
-                            
-               # timer = Timer(15.0, lambda: remove_partner(self))
-               # timer.start() s.bind(
-                #try:
-                  #  for i in range(0, len(self.partners)-1):
-                   #     if(self.partners[i][0] == username and self.partners[i][1] == addr):
-                    #        self.partners.remove(i)
+                        if ((IP, PORT) == addr):
+                            t = Timer(15.0, lambda: self.remove_partner(addr))
+                            t.daemon = True
+                            t.start()
+                            self.partners.append([addr, data[6:], t])
+                            print(self.partners)
 
-                     #       self.partners.append([username, addr, timer])
-                      #  else:
-                       #     self.partners.append([username, addr, timer])
-                #except:
-                 #   print ("No partners")
-
-                # Print all incoming messages that are not from ourselves
             else:
-                if(addr[1] != MYPORT+1):
-                    print(data)
-
+                for elem in self.partners:
+                    if elem[0] == addr:
+                        print((elem[1] + b': ' + data).decode())
 
 
 # Hello Thread.
 # Sends a "HELLO" message every 5 seconds so that other users on the
 # network can add to their active user lists
 class Hello(Thread):
-    def __init__(self, username):
+    def __init__(self, socket):
         Thread.__init__(self)
-        self.username = username
+        self.username = ''
+        self.socket = socket
 
     def hello(self):
         try:
-            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        except:
-            print ("Cannot open socket")
-            sys.exit(1)
-
-        try:
-            s.bind(('', MYPORT+1))
-        except:
-            print ("Cannot bind HELLO to port")
-            sys.exit(1)
-
-        try:
             string = str.encode('HELLO ' + self.username)
-           
             for IP in IPADDRESSRANGE:
                 for PORT in PORTRANGE:
-                    s.sendto(string, (IP, PORT))
+                    self.socket.sendto(string, (IP, PORT))
         except OSError as err:
             print ('Cannot send: {}'.format(err.strerror))
             sys.exit(1)
 
     def run(self):
-        while True:
-            sleep(5.0)
-            s.close()
+        while(True):
             self.hello()
+            sleep(5.0)
 
-def check_username(username):
-    check = True
-    for letter in username:
-        if(letter.isalnum() or letter=='_' or letter=='-' or letter=='.'):
+    def create_username(self):
+        check = False
+        while(check==False):
+            print("Reenter a valid username")
+            username = input('')
             check = True
-        else:
-            check = False
-            break
-    return check
+            for letter in username:
+                if (letter.isalnum() or letter=='_' or letter=='_' or letter=='.'):
+                    check = True
+                else:
+                    check = False
+                    break
+        self.username = username
+
 
 def main():
-    print("Please enter a username")
-    username = input('')
-    check = check_username(username)
-    while(check==False):
-        print("Reenter a valid unsername")
-        username = input('')
-        check = check_username(username)
-   
-    queue = Queue()
-    r = Receiver(queue, username)
+    try:
+        user_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    except:
+        print ("Cannot open socket")
+        sys.exit(1)
+
+    try:
+        user_socket.bind(('',MYPORT))
+        print("Port bound")
+    except:
+        print ("Cannot bind my socket to port")
+        sys.exit(1)
+
+    r = Receiver(user_socket)
     r.daemon = True
     r.start()
-    h = Hello(username)
+    h = Hello(user_socket)
     h.daemon = True
+    h.create_username()
     h.start()
 
     print ('s <msg> - sends message \nq-quits\n')
     cmd = input('')
     while cmd[0] != 'q':
         if cmd[0] is 's':
-            message = username+': '
+            message = ''
             for char in range(2, len(cmd)):
                 message += cmd[char]
 
-            chat_message = message
-            chat_message=json.dumps(chat_message).encode('utf-8')
-
-            try:
-                s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            except:
-                print("Cannot open socket")
-                sys.exit(1)
-            try:
-                s.bind(('', MYPORT+1))
-            except:
-                print("Cannot bind socket to port")
-               
-           # try:
-            #    for IP in IPADDRESSRANGE:
-             #       for PORT in PORTRANGE:
-              #          s.sendto(chat_message, (IP, PORT))
             try:
                 for partner in r.partners:
-                    print(partner)
-                    #send_message = str.encode(message)
-                    s.sendto(chat_message, (partner[0], partner[1]))
+                    send_message = str.encode(message)
+                    user_socket.sendto(send_message, partner[0])
             except OSError as err:
                 print('Cannot send: {}'.format(err.strerror))
                 sys.exit(1)
         cmd = input('')
 
-print('So long partner')
+        print('So long partner')
 
 
 main()
+
